d4bffa1a77995dcfe05e9579ed45cd833b8b668e
Renato Faria <raf31283@gmail.com>
2024-02-11 14:16:27 -0300

    Adicionada estrutura básica de HTML, CSS e JS para o cálculo do IMC

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+function entendaCalc() {
+  alert('O Índice de Massa Corporal (IMC) é uma medida utilizada para avaliar se o peso de uma pessoa está dentro do valor ideal para a altura. Ele é calculado dividindo o peso (em quilogramas) pela altura (em metros) ao quadrado. O resultado do IMC é dado em kg/m².');
+}
